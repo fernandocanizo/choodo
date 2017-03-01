a247e9b58e6f9bc8f389d12863c9acb217682678
Fernando L. Canizo <fernandocanizo@gmail.com>
2017-03-01 00:17:27 -0300

    Add working very basic non-interactive view

---
+++ index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const choo = require('choo');
+const html = require('choo/html');
+const app = choo();
+
+
+app.model({
+  state: {
+    todos: [
+      { title: 'Buy milk' },
+      { title: 'Call Samantha'},
+    ],
+  },
+});
+
+const view = (state, prevState, send) => {
+  return html`
+    <div>
+      <h1>ChooDo</h1>
+      <ul>
+        ${state.todos.map((todo) => html`<li>${todo.title}</li>`)}
+      </ul>
+    </div>
+`;
+}
+
+app.router([
+  ['/', view],
+]);
+
+const tree = app.start();
+document.body.appendChild(tree);
