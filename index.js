1632c5c9dde8dce432cfa9e65f0d39cc76c07c4b
Fernando L. Canizo <fernandocanizo@gmail.com>
2017-03-03 00:21:08 -0300

    Add local storage wrapper

--- index.js
+++ index.js
@@ -5,6 +5,32 @@ const choo = require('choo');
 const html = require('choo/html');
 const app = choo();
 
+// localStorage wrapper
+const store = {
+  getAll: (storeName, cb) => {
+    try {
+      cb(JSON.parse(window.localStorage[storeName]));
+    } catch (e) {
+      cb([]);
+    }
+  },
+
+  add: (storeName, item, cb) => {
+    store.getAll(storeName, (items) => {
+      items.push(item);
+      window.localStorage[storeName] = JSON.stringify(items);
+      cb();
+    });
+  },
+
+  replace: (storeName, index, item, cb) => {
+    store.getAll(storeName, (items) => {
+      items[index] = item;
+      window.localStorage[storeName] = JSON.stringify(items);
+      cb();
+    });
+  },
+};
 
 app.model({
   state: {
